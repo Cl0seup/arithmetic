cf93703f12cf2894d740d2fb71429f21c9f8929d
Cl0seup <sravan.kumar1691@gmail.com>
2018-02-06 16:33:58 +0530

    complete

--- arithmetic.js
+++ arithmetic.js
@@ -3,8 +3,17 @@ a = prompt("Enter a value");
 
 document.write("<div class='style'>");
 
-document.write("Given value is :-" + a);
+document.write("Given value is :-" + " " + a);
 document.write("<br>" + "<br>");
+
+function numbers() {
+	document.write("Numbers from 1 to " + a + " :-" + " ");
+	for (i = 1; i <= a; i++) {
+		document.write(i + ",");
+	}
+	document.write("<br>" + "<br>");
+}
+numbers();
 
 function table() {
 	document.write("Table is :-" + "<br>")
@@ -15,38 +24,29 @@ function table() {
 table();
 
 function square() {
-	document.write("<br>" + "Square is :-" + " " + a * a, "<br>");
+	document.write("<br>" + "Square is :-" + "  " + a * a, "<br>");
 }
 square();
 
 document.write("<br>" + " Square root is :-" + " " + Math.sqrt(a) + "<br>" + "<br>");
 
-function numbers() {
-	document.write("Numbers are :-" + " ");
-	for (i = 1; i <= a; i++) {
-		document.write(i + ",");
-	}
-	document.write("<br>" + "<br>");
-}
-numbers();
-
+/*
 function addition() {
 
-	document.write("Addition of numbers from 1 to" + " " + a + ":" + " ");
-
-	for (i = 1; i <= a; i++) {
-		document.write(i);
+	document.write("Addition of numbers from 1 to" + " " + a + ":-" + " ");
+	for (a = 1; a <= 100; a++) {
+		document.write(n[a]);
 	}
 	document.write("<br>" + "<br>");
 
 }
 addition();
-
+*/
 function numbertype() {
 	if (a % 2 == 0) {
-		document.write("Number is Prime number:-" + " " + a);
+		document.write("This is  a prime number:-" + " " + a);
 	} else if (a % 2 != 0) {
-		document.write("Number is Odd number :-" + " " + a);
+		document.write("This is an Odd number :-" + " " + a);
 	}
 	document.write("<br>" + "<br>");
 }
