6eb58b9a02d96b844aa33190114aab88ee92e85f
Cl0seup <sravan.kumar1691@gmail.com>
2018-02-06 11:39:33 +0530

    edited numbers

--- task3.js
+++ task3.js
@@ -1,39 +1,49 @@
  var a;
  a = prompt("enter a value");
- document.write("<div>");
-
- for (i = 1; i <= 10; i++) {
-     document.write(a + "*" + i + "=");
-     document.write(a * i, "<br>");
- }
- document.write("</div>");
+ /* document.write("<div>");
+  for (i = 1; i <= 10; i++) {
+      document.write(a + "*" + i + "=");
+      document.write(a * i, "<br>");
+  }
+  document.write("</div>");*/
 
  function table() {
+     document.write("table is :" + "<br>")
      for (i = 1; i <= 10; i++) {
          document.write(a + "*" + i + "=", a * i, "<br>");
      }
  }
- document.write("table is :" + "<br>")
+
  table();
 
  function square() {
      document.write("<br>" + "square is :", +a * a, "<br>");
  }
  square();
- document.write("<br>");
- document.write(" square root is :");
- document.write(Math.sqrt(a), "<br>");
- document.write("<br>");
- document.write("Numbers are :" + "<br>");
+ document.write("<br>" + " square root is :" + Math.sqrt(a) + "<br>" + "<br>");
 
  function numbers() {
-     for (i = 1; i <= a; i++)
-         document.write(i + "<br>");
+     document.write("Numbers are :");
+     for (i = 1; i <= a; i++) {
+         document.write(i + ",");
+     }
+     document.write("<br>" + "<br>");
  }
  numbers();
 
- function check() {
-
-
-
+ function primenumbers() {
+     document.write("Prime numbers are :");
+     for (i = 2; i <= a; i += 2) {
+         document.write(i + ",");
+     }
+     document.write("<br>" + "<br>");
  }
+ primenumbers();
+
+ function oddnumbers() {
+     document.write("Odd numbers  are :");
+     for (i = 2; i <= a; i += 1) {
+         document.write(i + ",");
+     }
+ }
+ oddnumbers();
