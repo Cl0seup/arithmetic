a89dca1e84a73ce673b3526c9ce053ea35354a2e
Cl0seup <sravan.kumar1691@gmail.com>
2018-02-07 22:11:18 +0530

    more to do

--- arithmetic.js
+++ arithmetic.js
@@ -1,14 +1,15 @@
 var a;
-a = prompt("Enter a value");
+a = parseFloat(prompt("Enter a value"));
 document.write("<div>")
-document.write("Given value is :-" + " " + a);
+document.write("Given value is :-" + " " + a + " " + ".");
 document.write("<br>" + "<br>");
 
 function numbers() {
-	document.write("Numbers from 1 to " + a + " :-" + " ");
-	for (i = 1; i <= a; i++) {
-		document.write(i + " " + ",");
+	document.write("Numbers from 1 to " + a + " :-" + " " + " ");
+	for (i = 1; i < a; i++) {
+		document.write(i + " " + "," + " ");
 	}
+	document.write(a + " " + ".")
 	document.write("<br>" + "<br>");
 }
 numbers();
@@ -21,15 +22,17 @@ function table() {
 }
 table();
 
-document.write("<br>" + "Square is :-" + "  " + a * a, "<br>");
+document.write("<br>" + "Square is :-" + "  " + a * a + " " + "." + "<br>");
 
-document.write("<br>" + " Square root is :-" + " " + Math.sqrt(a) + "<br>" + "<br>");
+document.write("<br>" + " Square root is :-" + " " + Math.sqrt(a) + " " + "." + "<br>" + "<br>");
 
 function numbertype() {
 	if (a % 2 == 0) {
-		document.write("This is  a prime number:-" + " " + a);
+		document.write("This is  a prime number:-" + " " + a + " " + ".");
 	} else if (a % 2 != 0) {
-		document.write("This is an odd number :-" + " " + a);
+		document.write("This is an odd number :-" + " " + a + " " + ".");
+	} else if (a == 0) {
+		document.write(" Please enter a valid number")
 	}
 	document.write("<br>" + "<br>");
 }
@@ -37,18 +40,23 @@ numbertype();
 
 function primenumbers() {
 	document.write("Prime numbers are :-" + " ");
-	for (i = 2; i <= a; i += 2) {
-		document.write(i + " " + ",");
+	for (i = 2; i < a; i += 2) {
+		document.write(i + " " + "," + " ");
 	}
-
+	if (a % 2 == 0) {
+		document.write(a + " " + ".");
+	}
 	document.write("<br>" + "<br>");
 }
 primenumbers();
 
 function oddnumbers() {
 	document.write("Odd numbers  are :-" + " ");
-	for (i = 1; i <= a; i += 2) {
-		document.write(i + " " + ",");
+	for (i = 1; i < a; i += 2) {
+		document.write(i + " " + "," + " ");
+	}
+	if (a % 2 != 0) {
+		document.write(a + " " + ".");
 	}
 	document.write("<br>" + "<br>");
 }
