92e08945bacf439a72e1391ea70ed5abfbf07002
Cl0seup <sravan.kumar1691@gmail.com>
2018-02-07 21:28:51 +0530

    edited bal

--- arithmetic.js
+++ arithmetic.js
@@ -1,12 +1,13 @@
 var a;
 a = prompt("Enter a value");
+document.write("<div>")
 document.write("Given value is :-" + " " + a);
 document.write("<br>" + "<br>");
 
 function numbers() {
 	document.write("Numbers from 1 to " + a + " :-" + " ");
 	for (i = 1; i <= a; i++) {
-		document.write(i + ",");
+		document.write(i + " " + ",");
 	}
 	document.write("<br>" + "<br>");
 }
@@ -37,7 +38,7 @@ numbertype();
 function primenumbers() {
 	document.write("Prime numbers are :-" + " ");
 	for (i = 2; i <= a; i += 2) {
-		document.write(i + ",");
+		document.write(i + " " + ",");
 	}
 
 	document.write("<br>" + "<br>");
@@ -47,8 +48,9 @@ primenumbers();
 function oddnumbers() {
 	document.write("Odd numbers  are :-" + " ");
 	for (i = 1; i <= a; i += 2) {
-		document.write(i + ",");
+		document.write(i + " " + ",");
 	}
 	document.write("<br>" + "<br>");
 }
 oddnumbers();
+document.write("</div>")
